44ce3f548c736775b1c80ed01cbe559859ecd23b
movfel <42951278+movfel@users.noreply.github.com>
2018-12-04 11:56:19 -0500

    js

--- js/site.js
+++ js/site.js
@@ -1,51 +1,14 @@
-/*
-  Returns a random whole number between minimum value and maximum value
-
-  For example, calling randomNumber(0, 255);
-  will return a random whole number between 0 and 255.
-
-  Source: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-*/
-function randomNumber (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-/*
-  Returns a random floating point number between minimum value and maximum value
-
-  For example, calling randomFloat(0, 1);
-  will return a random floating between 0.0 and 1.0.
-
-  Source: https://stackoverflow.com/questions/9724404/random-floating-point-double-in-inclusive-range
-*/
-function randomFloat (min, max) {
-  var float = Math.random();
-  var value;
-
-  if (float < 0.5) {
-    value = (1 - Math.random()) * (max-min) + min;
-  } else {
-    value = Math.random() * (max-min) + min;
-  }
-
-  return parseFloat(value.toFixed(2));
-}
-
-
-/*
-  Returns a color string in the form of "hsl(100, 50%, 50%)"
-  For example, calling color(10, 100, 40);
-  will return a string "hsl(10, 100%, 40%)"
-*/
-function hslColor(h, s, l) {
-  var hue = h;
-  var saturation = s + "%";
-  var luminance = l + "%";
-
-  var color = "hsl(" + hue + "," + saturation + "," + luminance + ")";
-
-  return color;
-}
-
-
-/* START WRITING YOUR CODE BELOW */
+$(".stripe").mousemove(function(event) {
+  var eye = $(".eye");
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  console.log(rot);
+  eye.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+})
